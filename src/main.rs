82cc40ee36382dabc29d5f2e98f1c8191a7f16f6
Julian Theis <jlntheis@gmail.com>
2019-06-02 00:42:21 +0200

    Generate command string from image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate image;
 
-use image::GenericImageView;
+use image::{GenericImageView, DynamicImage, Pixel};
 
 fn main() {
     let filename = std::env::args().nth(1);
@@ -24,4 +24,19 @@ fn main() {
 
     let (x, y) = img.dimensions();
     println!("{}x{}", x, y);
+
+    let cmd = generate_command(&img);
+    println!("Generated command:\n{}", cmd);
+}
+
+fn generate_command(img: &DynamicImage) -> String {
+    let mut cmd = String::from("");
+    for (x, y, pix) in img.pixels() {
+        let rgba = pix.to_rgba();
+        if rgba[0] != 0 {
+            cmd.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x, y, rgba[0], rgba[1], rgba[2], rgba[3]));
+        }
+    }
+
+    cmd
 }
