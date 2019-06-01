b2cea558e4ebf15c2b08910db1269cb5133d1da4
Julian Theis <jlntheis@gmail.com>
2019-06-01 13:40:44 +0200

    Make output look nice on error

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,24 @@ extern crate image;
 use image::GenericImageView;
 
 fn main() {
-    let filename = std::env::args().nth(1).expect("No filename given");
-    let img = image::open(filename).expect("Could not open image");
+    let filename = std::env::args().nth(1);
+    let filename = match filename {
+        Some(x) => x,
+        None => {
+            println!("No filename given");
+            std::process::exit(0);
+        },
+    };
+
+    let img = image::open(&filename);
+    let img = match img {
+        Ok(x) => x,
+        Err(error) => {
+            println!("Could not open {}: {}", &filename, error);
+            std::process::exit(0);
+        },
+    };
+
 
     let (x, y) = img.dimensions();
     println!("{}x{}", x, y);
