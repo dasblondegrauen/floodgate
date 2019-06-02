ca88bccde2af39a58ff3e26e09729ab2ed3eca75
Julian Theis <jlntheis@gmail.com>
2019-06-02 22:51:10 +0200

    Add function to parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 extern crate image;
 
 use image::{GenericImageView, DynamicImage, Pixel};
-use std::net::{UdpSocket};
+use std::net::UdpSocket;
 
-fn main() {
-    let filename = std::env::args().nth(1);
-    let filename = match filename {
+enum ArgumentType {
+    Required(String),
+    Optional(String),
+}
+
+fn get_argument(position: usize, argument_type: ArgumentType) -> String {
+    match std::env::args().nth(position) {
         Some(value) => value,
         None => {
-            println!("No filename given");
-            std::process::exit(0);
-        },
-    };
+            match argument_type {
+                ArgumentType::Required(error_message) => {
+                    println!("{}", &error_message);
+                    std::process::exit(0);
+                },
+                ArgumentType::Optional(default_value) => default_value.clone(),
+            }
+        }
+    }
+}
+
+fn generate_command(img: &DynamicImage, offset: (u32, u32)) -> String {
+    let mut cmd = String::from("");
+    for (x, y, pix) in img.pixels() {
+        let rgba = pix.to_rgba();
+        if rgba[0] != 0 {
+            cmd.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x+offset.0, y+offset.1, rgba[0], rgba[1], rgba[2], rgba[3]));
+        }
+    }
+
+    cmd
+}
+
+fn main() {
+    let filename = get_argument(1, ArgumentType::Required(String::from("No filename given")));
 
     let target = std::env::args().nth(2);
     let target = match target {
@@ -42,16 +67,4 @@ fn main() {
             i = 0;
         }
     }
-}
-
-fn generate_command(img: &DynamicImage, offset: (u32, u32)) -> String {
-    let mut cmd = String::from("");
-    for (x, y, pix) in img.pixels() {
-        let rgba = pix.to_rgba();
-        if rgba[0] != 0 {
-            cmd.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x+offset.0, y+offset.1, rgba[0], rgba[1], rgba[2], rgba[3]));
-        }
-    }
-
-    cmd
 }
