22839adc245bc1665182eeb08b78cb58ee5b01f0
Julian Theis <jlntheis@gmail.com>
2019-12-28 13:30:47 +0100

    Read/print CLI parameters

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,17 @@ fn main() {
     let matches = App::from_yaml(clap_yml).get_matches();
 
     if let Some(host) = matches.value_of("host") {
-        println!("Pixelflut host: {}", host);
+        println!("Pixelflut server address: {}", host);
     } else {
-        println!("No host given");
+        println!("No server address specified");
+    }
+
+    let port = matches.value_of("port").unwrap_or_default();
+    println!("Pixelflut server port: {}", port);
+
+    if let Some(image) = matches.value_of("image") {
+        println!("Image file: {}", image);
+    } else {
+        println!("No image file specified");
     }
 }
