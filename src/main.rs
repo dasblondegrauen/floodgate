99a54056ad6b5089d05800dbfcd3e404f9003397
Julian Theis <jlntheis@gmail.com>
2019-12-29 23:17:13 +0100

    Remove debug output

--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,12 @@ fn main() {
     let matches = App::from_yaml(clap_yml).get_matches();
 
     if let Some(host) = matches.value_of("host") {
-        println!("Pixelflut server address: {}", host);
-
         let port = matches.value_of("port").unwrap_or_default();
-        println!("Pixelflut server port: {}", port);
 
         if let Some(subcommand_matches) = matches.subcommand_matches("picture") {
             if let Some(filename) = subcommand_matches.value_of("file") {
-                println!("Picture file: {}", filename);
                 let generator = PictureGenerator::load_picture(String::from(filename));
                 let dimensions = generator.get_image().dimensions();
-                println!("{}x{}", dimensions.0, dimensions.1);
             } else {
                 println!("No picture file specified");
             }
