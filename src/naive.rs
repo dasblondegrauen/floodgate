9568a3fef986cc1c37c6838aa327fdc49f6b4a9d
Julian Theis <jlntheis@gmail.com>
2019-12-30 00:09:29 +0100

    Render command in struct immediately

--- src/naive.rs
+++ src/naive.rs
@@ -7,17 +7,15 @@ pub struct NaiveRenderer {
 
 impl Renderer for NaiveRenderer {
     fn render_command(&mut self, frame: &DynamicImage) {
-        let mut cmd = String::new();
+        self.command = String::new();
 
         for (x, y, pix) in frame.pixels() {
             let rgba = pix.to_rgba();
             if rgba[0] != 0 {
                 // cmd.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x+offset.0, y+offset.1, rgba[0], rgba[1], rgba[2], rgba[3]));
-                cmd.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x, y, rgba[0], rgba[1], rgba[2], rgba[3]));
+                self.command.push_str(&format!("PX {} {} {:02X}{:02X}{:02X}{:02X}\n", x, y, rgba[0], rgba[1], rgba[2], rgba[3]));
             }
         }
-
-        self.command = cmd;
     }
 
     fn get_command(&self) -> String {
